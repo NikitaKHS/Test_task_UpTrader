05b3afc3fedae47f37ec09d290168852a937ab84
NikitaKHS <ask.khokhlov@gmail.com>
2025-04-28 01:50:09 +0300

    Поправил небольшой затуп

--- tree_menu/templatetags/menu_tags.py
+++ tree_menu/templatetags/menu_tags.py
@@ -1,36 +1,38 @@
 from django import template
-from tree_menu.models import Menu
+from tree_menu.models import MenuItem
 
 register = template.Library()
 
 def build_tree(items):
     nodes = {i.id: {'i': i, 'kids': []} for i in items}
     roots = []
-    for n in nodes.values():
-        pid = n['i'].parent_id
+    for node in nodes.values():
+        pid = node['i'].parent_id
         if pid and pid in nodes:
-            nodes[pid]['kids'].append(n)
+            nodes[pid]['kids'].append(node)
         else:
-            roots.append(n)
+            roots.append(node)
     return roots
 
 def mark_active(nodes, path):
-    for n in nodes:
-        n['cur'] = (n['i'].get_url() == path)
-        if n['kids']:
-            mark_active(n['kids'], path)
-            n['open'] = any(c.get('cur') or c.get('open') for c in n['kids']) or n['cur']
+    for node in nodes:
+        node['cur'] = (node['i'].get_url() == path)
+        if node['kids']:
+            mark_active(node['kids'], path)
+            node['open'] = any(c.get('cur') or c.get('open') for c in node['kids']) or node['cur']
         else:
-            n['open'] = n['cur']
+            node['open'] = node['cur']
 
 @register.inclusion_tag('tree_menu/menu.html', takes_context=True)
 def draw_menu(context, name):
-    req = context['request']
-    try:
-        m = Menu.objects.get(name=name)
-    except Menu.DoesNotExist:
+    request = context.get('request')
+    if not request:
         return {'nodes': []}
-    items = list(m.items.select_related('parent'))
+    items = list(
+        MenuItem.objects
+            .filter(menu__name=name)
+            .select_related('parent')
+    )
     tree = build_tree(items)
-    mark_active(tree, req.path)
+    mark_active(tree, request.path)
     return {'nodes': tree}
